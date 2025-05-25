a39d66561f76cc88b1566d468da90c967018eb30
NoosratMeem <meem.noosrat@gmail.com>
2025-05-25 20:56:38 +0600

    Problem 1: Mojo and Mutki Exchange Calculation has been solved by using javascript function

---
+++ problem-1.js
@@ -0,0 +1,16 @@
+// Problem 1: Mojo and Mutki Exchange Calculation
+
+function TotalConsumedMojos(initial_mojos){
+    let mutki = initial_mojos
+    let extra_mojos = 0 // extra mojos after consuming initial mojos
+
+    while(mutki >=3){
+        let new_mojo_taken_by_mutki =  Math.floor(mutki / 3) // mojo got by exchanging mutki
+        mutki = mutki - (new_mojo_taken_by_mutki  * 3) + new_mojo_taken_by_mutki // updated mutki after exchanging 
+        extra_mojos = extra_mojos + new_mojo_taken_by_mutki 
+    }
+
+    let consumed_mojos =  initial_mojos + extra_mojos
+    return consumed_mojos
+}
+console.log("Total Mojo Consumed: ", TotalConsumedMojos(10)) // result = 14
---
+++ problem-2.js
@@ -0,0 +1,2 @@
+// Problem 2: Inventory Management with Multiple Units
+
