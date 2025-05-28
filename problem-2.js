637b7d05423899ccd51021031a9a062d0433dfee
NoosratMeem <meem.noosrat@gmail.com>
2025-05-28 15:31:33 +0600

    problem 2 and problem 3 solution has beeen added

--- problem-2.js
+++ problem-2.js
@@ -1,2 +1,59 @@
 // Problem 2: Inventory Management with Multiple Units
 
+let initial_stock = {
+    "tons": 1,
+    "kilograms": 0,
+    "grams": 0,
+    "milligrams": 0
+}
+
+// converting all units in mg
+
+let ton = 1000000000
+let kilogram = 1000000
+let gram = 1000
+let milligram = 1
+
+
+initial_stock.tons = ton * initial_stock.tons
+initial_stock.kilograms = kilogram * initial_stock.kilograms
+initial_stock.grams = gram * initial_stock.grams
+initial_stock.milligrams = milligram * initial_stock.milligrams
+console.log("Initial Stock In MG:", initial_stock)
+
+
+let Total_in_milligrams = initial_stock.tons + initial_stock.kilograms + initial_stock.grams + initial_stock.milligrams
+
+
+// perform addition or subtraction
+// after sales
+let sales_in_mg = 1 * gram // sales in mg
+let update_stock = {}
+
+// converting back to prev units
+update_stock.tons = Math.floor((Total_in_milligrams - sales_in_mg) / ton )
+update_stock.kilograms = Math.floor(((Total_in_milligrams - sales_in_mg) % ton) / kilogram )
+update_stock.grams = Math.floor((((Total_in_milligrams - sales_in_mg) % ton) % kilogram) / gram )
+update_stock.milligrams = Math.floor(((((Total_in_milligrams - sales_in_mg) % ton) % kilogram) % gram) / milligram  )
+
+console.log("Updated Stock After Sale:", update_stock)
+
+
+// after purchase
+update_stock.tons = ton * update_stock.tons
+update_stock.kilograms = kilogram * update_stock.kilograms
+update_stock.grams = gram * update_stock.grams
+update_stock.milligrams = milligram * update_stock.milligrams
+console.log("Updated Stock In MG:", update_stock)
+
+
+let Total_Updated_Stock_in_milligrams = update_stock.tons + update_stock.kilograms + update_stock.grams + update_stock.milligrams
+
+let purchase_in_mg = 1001 * gram // purchase in mg.
+
+update_stock.tons = Math.floor((Total_Updated_Stock_in_milligrams + purchase_in_mg) / ton )
+update_stock.kilograms = Math.floor(((Total_Updated_Stock_in_milligrams + purchase_in_mg) % ton) / kilogram )
+update_stock.grams = Math.floor((((Total_Updated_Stock_in_milligrams + purchase_in_mg) % ton) % kilogram) / gram )
+update_stock.milligrams = Math.floor(((((Total_Updated_Stock_in_milligrams + purchase_in_mg) % ton) % kilogram) % gram) / milligram  )
+
+console.log("Updated Stock After Purchase:", update_stock)
---
+++ problem-3.js
@@ -0,0 +1,56 @@
+// // Problem 3: Message Processing with Retry Logic (Random ID Validation)
+
+const payments = [];
+
+function generateRandomTransaction() {
+  const trxid = Math.floor(Math.random() * 1000);
+  const amount = Math.floor(Math.random() * (1000 - 10 + 1)) + 10;
+
+  return {
+    trxid,
+    amount,
+    status: "pending",
+    attemptCount: 0,
+    timestamp: new Date(),
+    nextAttempAt: new Date(),
+  };
+}
+
+// Generate a new transaction every second
+setInterval(() => {
+  const message = generateRandomTransaction();
+  payments.push(message);
+  console.log("New Message Added", message);
+}, 1000);
+
+function netfeeCustomerRecharge(msg) {
+  console.log(`Recharge is done successfully for transaction Id ${msg.trxid}`);
+}
+
+function retry_with_delay(attempt) {
+  const delays = [2, 5, 10, 20, 30, 60];
+  return delays[attempt - 1] * 60 * 1000;
+}
+
+setInterval(() => {
+  const random_Id = Math.floor(Math.random() * 1000);
+
+  const now = new Date();
+
+  const nextMessage = payments.find(
+    (msg) =>
+      (msg.status === "pending" || msg.status === "rejected") &&
+      msg.nextAttempAt <= now
+  );
+  if (!nextMessage) return;
+  if (nextMessage.trxid === random_Id) {
+    nextMessage.status = "success";
+    netfeeCustomerRecharge(nextMessage);
+  } else {
+    nextMessage.status = "rejected";
+    nextMessage.attemptCount += 1;
+    const delay = retry_with_delay(nextMessage.attemptCount);
+    nextMessage.nextAttempAt = new Date(now.getTime() + delay);
+    // console.log("Log rejected message: ", payments);
+  }
+}, 4000);
